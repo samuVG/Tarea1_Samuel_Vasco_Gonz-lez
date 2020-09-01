9b8061c1d1530d8ac658597fde3177be9d839565
samuVG <65866495+samuVG@users.noreply.github.com>
2020-08-31 21:42:45 -0500

    Add files via upload

---
+++ Factorial(n).py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+def fact(n):
+    fac=1
+    for i in range (2,n+1):
+        fac=fac*i
+    return fac
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
